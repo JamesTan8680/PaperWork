474ee21b489cea48f8095554b201a9e8c5d7f889
huuhung7301 <48995642+huuhung7301@users.noreply.github.com>
2023-08-03 15:34:30 +1000

    Simon commit on main_screen backend code

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,38 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "note",
+  host: "database-2.co8ctu949l8t.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Paperwork-123",
+  database: "paperwork_project",
 });
-app.get("/", (req, res) => {
+app.get("/return_number_of_documents", (req, res) => {
   //write the query for the sql
-  const sql = "SELECT * FROM notes";
+  const sql = "SELECT count(*) AS 'total' FROM document_container";
   db.query(sql, (err, data) => {
     if (err) return res.send(err);
     return res.json(data);
   });
 });
+
+app.get("/return_most_popular_document", (req, res) => {
+  // Write the query for the SQL
+  const sql = "SELECT document_template_id, COUNT(*) AS count FROM document_container GROUP BY document_template_id ORDER BY count DESC LIMIT 1";
+  db.query(sql, (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.get("/return_recent_created_documents", (req, res) => {
+  // Write the query for the SQL
+  const sql = "SELECT document_container.document_template_id, document_template.version, document_container.issue_date FROM document_container INNER JOIN document_template ON document_container.document_template_id = document_template.document_template_id ORDER BY document_container.issue_date DESC LIMIT 5";
+  db.query(sql, (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
 
 //check if the database is existed or not
 db.connect(function (err) {
