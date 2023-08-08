427005a83d487672030dc45c7b8bedfd67ce800f
NguyenThang2 <87593511+NguyenThang2@users.noreply.github.com>
2023-08-09 02:29:37 +1000

    Get Dataa and show it ( Top Level )
    I got data from database then use axios library to get and use it

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Home.scss";
 import Doc from "../../img/home/document.svg";
 import Approval from "../../img/home/approval.svg";
 import { Link } from "react-router-dom";
-
-import Note from "../../components/Note/Note";
-import RecentDoc from "../../components/RecentDoc/RecentDoc";
 import Calendar from "../../components/Calendar/Calendar";
+import RecentDoc from "../../components/RecentDoc/RecentDoc";
+
+const BASE_URL = "http://localhost:8800/homepage";
+
 function Home() {
+  const [data, setData] = useState({
+    totalDocs: 0,
+    mostPopularDocs: [],
+    notes: [],
+  });
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const totalDocsResponse = await axios.get(
+          `${BASE_URL}/documents/total`
+        );
+        const mostPopularDocsResponse = await axios.get(
+          `${BASE_URL}/documents/most-popular`
+        );
+        const recentDocsResponse = await axios.get(
+          `${BASE_URL}/documents/recently-created`
+        );
+        const notesResponse = await axios.get(`${BASE_URL}/notes`);
+
+        setData({
+          totalDocs: totalDocsResponse.data[0].total,
+          mostPopularDocs: mostPopularDocsResponse.data,
+          notes: notesResponse.data,
+        });
+      } catch (err) {
+        console.error("Error during data fetching:", err);
+        setError("There was an error fetching the data. Please try again.");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div className="home">
       <div className="top">
@@ -17,22 +58,31 @@ function Home() {
             <div className="doc">
               <span>Documents</span>
               <div className="detail">
-                <img src={Doc} alt="" />
-                <span>53</span>
+                <img src={Doc} alt="Document Icon" />
+                <span>{data.totalDocs}</span>
               </div>
             </div>
           </Link>
           <div className="pop-doc">
             <span className="container-title">Most Popular Documents</span>
             <div className="detail">
-              <img src={Approval} alt="" />
-              <span className="doc-title">Non-disclosure Agreement</span>
+              <img src={Approval} alt="Approval Icon" />
+              {data.mostPopularDocs.map((doc, index) => (
+                <div key={index}>
+                  <span className="doc-title">{doc.document_template_id}</span>
+                </div>
+              ))}
             </div>
             <Link to="/createDoc">
               <button>Create Document</button>
             </Link>
           </div>
-          <Note />
+          {data.notes.map((note, index) => (
+            <div key={index}>
+              <h2>{note.header}</h2>
+              <p>{note.content}</p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="title">Note</div>
@@ -64,7 +114,9 @@ function Home() {
       <div className="title">Recent Created Documents</div>
       <div className="bottom">
         <div className="bottom-left">
-          <RecentDoc />
+          <div className="bottom-left">
+            <RecentDoc />
+          </div>
         </div>
         <div className="bottom-right">
           <Calendar />
