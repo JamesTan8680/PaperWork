6c595448e8c02f9a4bdd8eea4e28ee728b664788
Chingsien-102890973 <102890973@student.swin.edu.au>
2023-08-08 15:19:11 +1000

    Add the cors and convert the express.json

--- backend/index.js
+++ backend/index.js
@@ -9,84 +9,114 @@ const db = mysql.createConnection({
   database: "paperwork_project",
 });
 
-const app = express(); 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-function fetch(q, res){
+function fetch(q, res) {
   const sql = q;
-    db.query(sql, (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-
-    });
+  db.query(sql, (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 }
 
-function select(table, args, res){
+function select(table, args, res) {
   var q = "SELECT ";
   if (args.columns) q += args.columns;
   else q += "*";
-  q+= " FROM " + table;
-  if (args.other) q+= " " + args.other;
-  if (args.where) q+= " WHERE " + args.where;
-  if (args.groupBy) q+= " GROUP BY " + args.groupBy;
-  if (args.orderBy) q+= " ORDER BY " + args.orderBy;
+  q += " FROM " + table;
+  if (args.other) q += " " + args.other;
+  if (args.where) q += " WHERE " + args.where;
+  if (args.groupBy) q += " GROUP BY " + args.groupBy;
+  if (args.orderBy) q += " ORDER BY " + args.orderBy;
   console.log(q);
-  fetch(q,res);
+  fetch(q, res);
 }
 
 //get all documents by template
 app.get("/view-documents/templates", (req, res) => {
-  req.query.columns = "document_template.document_template_id AS id, document_template.title, COUNT(*) AS count";
-  req.query.other = "INNER JOIN document_default_template ON document_default_template.type = document_template.type GROUP BY document_template.type";
-  select("document_template",req.query,res);
+  req.query.columns =
+    "document_template.document_template_id AS id, document_template.title, COUNT(*) AS count";
+  req.query.other =
+    "INNER JOIN document_default_template ON document_default_template.type = document_template.type GROUP BY document_template.type";
+  select("document_template", req.query, res);
 });
 
 //get list of signatories of document
 //tempId: ID of the template type
 //docId: ID of the document
 app.get("/view-documents/templates/:tempId/:docId", (req, res) => {
-  req.query.where = "document_template.type = '" + req.params.tempId + "' AND document_template.document_template_id = '" + req.params.docId + "'";
-  req.query.other = "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
-  select("document_container",req.query,res);
+  req.query.where =
+    "document_template.type = '" +
+    req.params.tempId +
+    "' AND document_template.document_template_id = '" +
+    req.params.docId +
+    "'";
+  req.query.other =
+    "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
+  select("document_container", req.query, res);
 });
-
 
 app.get("/view-documents/templates/:id", (req, res) => {
-  req.query.where = "document_template.type = '" + req.params.id + "'"
- // req.query.other = "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
-  select("document_template",req.query,res);
+  req.query.where = "document_template.type = '" + req.params.id + "'";
+  // req.query.other = "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
+  select("document_template", req.query, res);
 });
-
 
 //This line separate Jordan code and Simon code --------------------------------------------------------------------------------------------
 //Simon`s code
 
 app.get("/homepage/documents/total", (req, res) => {
   //write the query for the sql
-  select("document_container",{"columns":"count(*) AS 'total'"},res);
+  select("document_container", { columns: "count(*) AS 'total'" }, res);
 });
 
 app.get("/homepage/documents/most-popular", (req, res) => {
   // Write the query for the SQL
- // const sql = "SELECT document_template_id, COUNT(*) AS count FROM document_container GROUP BY document_template_id ORDER BY count DESC LIMIT 1";
+  // const sql = "SELECT document_template_id, COUNT(*) AS count FROM document_container GROUP BY document_template_id ORDER BY count DESC LIMIT 1";
 
-  select("document_container",{"columns":"document_template_id, COUNT(*) AS count","orderBy":"count DESC","groupBy":"document_template_id","other":"LIMIT 1"},res);
-
+  select(
+    "document_container",
+    {
+      columns: "document_template_id, COUNT(*) AS count",
+      orderBy: "count DESC",
+      groupBy: "document_template_id",
+      other: "LIMIT 1",
+    },
+    res
+  );
 });
 
 app.get("/homepage/documents/recently-created", (req, res) => {
   // Write the query for the SQL, using DATE_FORMAT to format the date
   //const sql = "SELECT document_container.document_template_id, document_template.version, DATE_FORMAT(document_container.issue_date, '%d/%m/%Y') AS date_created FROM document_container INNER JOIN document_template ON document_container.document_template_id = document_template.document_template_id ORDER BY document_container.issue_date DESC LIMIT 5";
-  select("document_container",{"columns":"document_container.document_template_id, document_template.version, DATE_FORMAT(document_container.issue_date, '%d/%m/%Y') AS date_created"
-  ,"orderBy":"document_container.issue_date DESC","groupBy":"document_template_id","other":" INNER JOIN document_template ON document_container.document_template_id = document_template.document_template_id LIMIT 5"},res);
-
-
+  select(
+    "document_container",
+    {
+      columns:
+        "document_container.document_template_id, document_template.version, DATE_FORMAT(document_container.issue_date, '%d/%m/%Y') AS date_created",
+      orderBy: "document_container.issue_date DESC",
+      groupBy: "document_template_id",
+      other:
+        " INNER JOIN document_template ON document_container.document_template_id = document_template.document_template_id LIMIT 5",
+    },
+    res
+  );
 });
 
 app.get("/homepage/notes", (req, res) => {
   // Write the query for the SQL, using DATE_FORMAT to format the date
   //const sql = "SELECT note_id, DATE_FORMAT(date_created, '%d/%m/%Y') AS date_created, person_created, header, content from notes where is_removed = '0'";
-  select("notes",{"columns":"note_id, DATE_FORMAT(date_created, '%d/%m/%Y') AS date_created, person_created, header, content","where":"is_removed='0'"},res);
-
+  select(
+    "notes",
+    {
+      columns:
+        "note_id, DATE_FORMAT(date_created, '%d/%m/%Y') AS date_created, person_created, header, content",
+      where: "is_removed='0'",
+    },
+    res
+  );
 });
 
 //check if the database is existed or not
@@ -97,8 +127,7 @@ db.connect(function (err) {
   console.log("Connected to the MySQL server.");
 });
 
-  //listen to the backend
-  app.listen(8800, () => {
-    console.log("backend connected");
-  });
-  
+//listen to the backend
+app.listen(8800, () => {
+  console.log("backend connected");
+});
