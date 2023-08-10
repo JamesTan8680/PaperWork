d516905a2c291ef83450584cfe79e29746985950
Jordan Ferrazza <jordanferrazza@gmail.com>
2023-08-10 13:48:03 +1000

    Jordan made changes
    Added search
    Changed naming convention of some endpoints to match Simon

--- backend/index.js
+++ backend/index.js
@@ -33,33 +33,56 @@ function select(table, args, res) {
   fetch(q, res);
 }
 
+
+//create a search string, automatically creating an OR statement for different columns to search
+//prefix: (optional else use "") the operator to put in front of the string WITH SPACES
+//search: the string to search for
+//inArgs: an array of column name strings to search in
+function generateSearchString(prefix, search, inArgs){
+  var s = prefix;
+  s += "(";
+  for (var i in inArgs){
+    s += inArgs[i] + " LIKE '%" + search + "%'";
+    if (i != inArgs.length - 1) s += " OR ";
+  }
+  s += ")";
+  return s;
+}
+
 //get all documents by template
-app.get("/viewDocument/documentTemplate", (req, res) => {
+app.get("/view-document/document-template", (req, res) => {
   req.query.columns =
-    "document_template.document_template_id AS id, document_template.title, COUNT(*) AS count";
+    "document_template.document_template_id AS id, document_template.title, document_default_template.type, document_default_template.title AS type_name, COUNT(*) AS count";
   req.query.other =
-    "INNER JOIN document_default_template ON document_default_template.type = document_template.type GROUP BY document_template.type";
+    "INNER JOIN document_default_template ON document_default_template.type = document_template.type";
+  req.query.groupBy =
+    "document_template.type";
+  if (req.query.search) req.query.where = generateSearchString("", req.query.search, ["document_template.title","document_default_template.title"]);
   select("document_template", req.query, res);
 });
 
 //get list of signatories of document
 //tempId: ID of the template type
 //docId: ID of the document
-app.get("/viewDocument/documentTemplate/:tempId/:docId", (req, res) => {
+app.get("/view-document/document-template/:tempId/:docId", (req, res) => {
   req.query.where =
     "document_template.type = '" +
     req.params.tempId +
     "' AND document_template.document_template_id = '" +
     req.params.docId +
     "'";
+    if (req.query.search) req.query.where += generateSearchString(" AND ", req.query.search, ["document_template.title", "CONVERT(signed_date, CHAR)", "CONVERT(issue_date, CHAR)"]);
+
   req.query.other =
     "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
   select("document_container", req.query, res);
 });
 
-app.get("/viewDocument/documentTemplate/:id", (req, res) => {
+//get list of documents by type
+app.get("/view-document/document-template/:id", (req, res) => {
   req.query.where = "document_template.type = '" + req.params.id + "'";
   // req.query.other = "INNER JOIN document_template ON document_template.document_template_id = document_container.document_template_id";
+  if (req.query.search) req.query.where += generateSearchString(" AND ", req.query.search, ["document_template.title", "content"]);
   select("document_template", req.query, res);
 });
 
