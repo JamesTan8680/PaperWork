89a29a8a1d59224807fccbbc01a3e514401c57d7
huuhung7301 <48995642+huuhung7301@users.noreply.github.com>
2023-08-14 20:34:50 +1000

    update on create documents routes

--- backend/endpoints/createdocument_eps.js
+++ backend/endpoints/createdocument_eps.js
@@ -8,57 +8,39 @@ createdocument_ep_router.use(cors());
 createdocument_ep_router.use(express.json());
 const macros = new ep_macros();
 
-createdocument_ep_router.get("/templates", (req, res) => {
-    macros.query("SELECT * FROM document_default_template",res);
+createdocument_ep_router.get("/default-templates", (req, res) => {
+  const query =
+    "SELECT type, title, content FROM document_default_template";
+
+  db.query(query, (error, results) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+
+    const dataArray = [];
+    let idCounter = 1; // Initialize the counter at 1
+
+    results.forEach((result) => {
+      const dataObject = {
+        id: idCounter++, // Increment the counter and assign it as id
+        docTitle: result.title,
+        template: {
+          type: result.type,
+          term: result.content,
+        },
+        defaultTemplate: {
+          type: `${result.type} (Default Template)`,
+          term: "",
+        },
+      };
+
+      dataArray.push(dataObject);
+    });
+
+    res.json(dataArray);
+  });
 });
-
-
-// createdocument_ep_router.get("/templates", (req, res) => {
-//    const sql = "SELECT document_template_id AS id, document_template.title AS doc_title, document_template.type AS default_template, document_default_template.title, version, document_template.content FROM document_template INNER JOIN document_default_template ON document_default_template.type = document_template.type";
-//    db.query(sql, (err, data) => {
-//        if (err) return res.send(err);
-
-
-//     data.forEach(result => {
-//         const dataArray = [];
-
-//         results.forEach(result => {
-//           const dataObject = {
-//             id: result.id,
-//             docTitle: result.doc_title,
-//             template: {
-//               type: result.template_type,
-//               term: result.content,
-//             },
-//             defaultTemplate: {
-//               type: result.template_type,
-//               term: '',
-//             },
-//           };
-    
-//           dataArray.push(dataObject);
-//         });
-//     });
-//        return res.json(data);
-//    });
-// });
-  
-
-//Return object list of all version-template
-createdocument_ep_router.get("create-document/template-version", (req, res) => {
-    //write the query for the sql
-    fetch("SELECT count(*) AS 'total' FROM document_default_template", res);
-  });
-  
-  
-  //Return term base on the template id and version id for preview
-  createdocument_ep_router.get("create-document/:template/:version", (req, res) => {
-    // Write the query for the SQL
-    fetch(
-      "SELECT title, COUNT(*) AS count FROM document_template GROUP BY type ORDER BY count DESC LIMIT 1",
-      res
-    );
-  });
-  
 
 export default createdocument_ep_router;
