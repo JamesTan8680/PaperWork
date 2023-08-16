38e0a6fe9cb0ff3d147d19f2ab5a33583f3f4338
NguyenThang2 <87593511+NguyenThang2@users.noreply.github.com>
2023-08-17 00:30:51 +1000

    finish sprint 2
    get data from database ( create document )
    add function remove angle bracket to remove angle backet in html

--- client/src/pages/CreateDoc/CreateDoc.jsx
+++ client/src/pages/CreateDoc/CreateDoc.jsx
@@ -1,65 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./CreateDoc.scss";
 import Temp from "../../img/createDoc/template.svg";
 import Ver from "../../img/createDoc/version.svg";
-
 import ReviewTemplate from "../../components/ReviewTemplate/ReviewTemplate";
+import RemoveAngleBrackets from "../../components/RemoveAngleBrackets/RemoveAngleBrackets";
 
 function CreateDoc() {
   //create the dummy data that will replace by the data in the database
-  const data = [
-    {
-      id: "1",
-      docTitle: "NON-DISCLOSURE AGREEMENT  ",
-      template: {
-        type: "NDA (Defult Template)",
-        term: "This document is confidentail",
-      },
-      defaultTemplate: { type: "NDA (Defult Template)", term: "" },
-    },
-    {
-      id: "2",
-      docTitle: "Security policy",
-      template: {
-        type: "SP (Defult Template",
-        term: "This document is confidentail",
-      },
-      defaultTemplate: { type: "SP (Defult Template)", term: "" },
-    },
-    {
-      id: "3",
-      docTitle: "Healt Agreement",
-      template: {
-        type: "HA (Defult Template",
-        term: "This document is confidentail",
-      },
-      defaultTemplate: { type: "HA (Defult Template)", term: "" },
-    },
-    {
-      id: "4",
-      docTitle: "Work hour Agreement",
-      template: {
-        type: "WH (Defult Template",
-        term: "This document is confidentail",
-      },
-      defaultTemplate: { type: "WH (Defult Template)", term: "" },
-    },
-  ];
-  //handle the select
-  const [templateSelect, setTemplateSelect] = useState();
-
-  //state to manage the data for the template
+  const [data, setData] = useState([]);
+  const [templateSelect, setTemplateSelect] = useState(null); // Initialize with null because data is initially empty
   const [template, setTemplate] = useState("default");
 
-  //handle the click to change the template type
-  const handleClickType = (type, id) => {
-    setTemplate(type);
-  };
-  //handle select for the template
-  const handleSelectTemplate = (id) => {
-    setTemplateSelect(id);
-  };
+  useEffect(() => {
+    const apiUrl = "http://localhost:8800/create-document/default-templates";
 
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data);
+        console.log(data);
+        if (!templateSelect) {
+          // If templateSelect is not set, then set it to the first item's id
+          setTemplateSelect(response.data[0]?.id);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [templateSelect]);
+
+  // Define selectedTemplateData outside of useEffect
+  const selectedTemplateData = data.find((item) => item.id === templateSelect);
+
+  // Handlers
+  const handleClickType = (type) => setTemplate(type);
+  const handleSelectTemplate = (id) => setTemplateSelect(id);
   return (
     <div className="createDoc">
       <span className="title">Create Document Page</span>
@@ -70,20 +46,19 @@ function CreateDoc() {
             Template Type
           </div>
           <div className="list-container">
-            {data.map((item) => {
-              return (
-                <span
-                  className={
-                    templateSelect === item.id
-                      ? "list-createDoc selected"
-                      : "list-createDoc"
-                  }
-                  onClick={() => handleSelectTemplate(item.id)}
-                  key={item.id}>
-                  {item.docTitle}
-                </span>
-              );
-            })}
+            {data.map((item) => (
+              <span
+                className={
+                  templateSelect === item.id
+                    ? "list-createDoc selected"
+                    : "list-createDoc"
+                }
+                onClick={() => handleSelectTemplate(item.id)}
+                key={item.id}
+              >
+                <RemoveAngleBrackets input={item.docTitle} />
+              </span>
+            ))}
           </div>
         </div>
         <div className="mid-createDoc">
@@ -94,19 +69,21 @@ function CreateDoc() {
           <div className="list-container">
             <div
               onClick={() => handleClickType("default")}
-              className={template === "default" ? "selected" : ""}>
+              className={template === "default" ? "selected" : ""}
+            >
               Default Template
             </div>
             <div
               onClick={() => handleClickType("blank")}
-              className={template === "blank" ? "selected" : ""}>
+              className={template === "blank" ? "selected" : ""}
+            >
               {" "}
               Blank Template
             </div>
           </div>
         </div>
         <div className="right-createDoc">
-          <ReviewTemplate type={template} />
+          <ReviewTemplate type={template} templateData={selectedTemplateData} />
         </div>
       </div>
     </div>
