0ea5404e02b3bfd330630a8bc8cfd734f5c7bc92
huuhung7301 <48995642+huuhung7301@users.noreply.github.com>
2023-08-22 00:15:13 +1000

    4 routes for create document - Simon

--- backend/endpoints/createdocument_eps.js
+++ backend/endpoints/createdocument_eps.js
@@ -43,4 +43,92 @@ createdocument_ep_router.get("/default-templates", (req, res) => {
   });
 });
 
+createdocument_ep_router.get("/variables/:type", (req, res) => {
+  const type = req.params.type;
+  const query =
+    "SELECT id, var_name, var_hint FROM document_default_template_variables WHERE type = ?";
+
+  db.query(query, [type], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: "An error occurred while fetching data." });
+    }
+
+    return res.json(result);
+  });
+});
+
+createdocument_ep_router.post("/variables", (req, res) => {
+  const { type, var_name, var_hint } = req.body;
+
+  const query =
+    "INSERT INTO document_default_template_variables (type, var_name, var_hint) VALUES (?, ?, ?)";
+
+  db.query(query, [type, var_name, var_hint], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: "An error occurred while inserting the variable." });
+    }
+
+    return res.json({ message: "Variable inserted successfully" });
+  });
+});
+
+createdocument_ep_router.delete("/variables/:id", (req, res) => {
+  const varId = req.params.id;
+
+  const query = "DELETE FROM document_default_template_variables WHERE id = ?";
+
+  db.query(query, [varId], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: "An error occurred while deleting the variable." });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Variable not found" });
+    }
+
+    return res.json({ message: "Variable deleted successfully" });
+  });
+});
+
+
+createdocument_ep_router.post("/template", (req, res) => {
+  const { type, title, content, parties_number, created_date, var_list } = req.body;
+
+  // Query to get the latest version with the given type
+  const getLatestVersion =
+    "SELECT version FROM document_template WHERE type = ? ORDER BY version DESC LIMIT 1";
+
+  db.query(getLatestVersion, [type], (latest_version_err, latest_version_result) => {
+    if (latest_version_err) {
+      return res.status(500).json({ error: "An error occurred while querying for the latest version." });
+    }
+
+    let newNumber = 1; // Initialize the newNumber to 1 as default
+    if (latest_version_result.length > 0) {
+      const latest_version = latest_version_result[0].version;
+      newNumber = latest_version + 1; // Increment the latest version to get the new number
+    }
+
+    // Create the new template ID
+    const document_template_id = `${type}_${newNumber}`;
+
+    // Insert the new template
+    const insertQuery =
+    "INSERT INTO document_template (document_template_id, type, title, content, parties_number, created_date, var_list, version) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+    db.query(
+      insertQuery,
+      [document_template_id, type, title, content, parties_number, created_date, JSON.stringify(var_list), newNumber],
+      (insert_err, insert_result) => {
+        if (insert_err) {
+          return res.status(500).json({ error: "An error occurred while inserting the template." });
+        }
+        
+        return res.json({ message: "Template inserted successfully", document_template_id }); // Return the new ID
+      }
+    );
+  });
+});
+
+
 export default createdocument_ep_router;
