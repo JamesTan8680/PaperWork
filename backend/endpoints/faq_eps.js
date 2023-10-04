0037d89214e4fc06d8e549f738765a4827761471
Jordan Ferrazza <jordanferrazza@gmail.com>
2023-10-04 20:55:34 +1100

    Fix and extend FAQ endpoint

--- backend/endpoints/faq_eps.js
+++ backend/endpoints/faq_eps.js
@@ -1,3 +1,8 @@
+import express from "express";
+import cors from "cors";
+import db from "./db.js";
+import ep_macros from "./macro.js";
+
 const faq_ep_router = express.Router();
 faq_ep_router.use(cors());
 faq_ep_router.use(express.json());
@@ -5,6 +10,11 @@ const macros = new ep_macros();
 
 faq_ep_router.get("/:id", (req, res) =>{
     macros.query("SELECT * FROM faq WHERE faq_id = ?",[req.params.id],res)
+})
+
+
+faq_ep_router.get("/", (req, res) =>{
+    macros.query("SELECT * FROM faq",res)
 })
 
 
